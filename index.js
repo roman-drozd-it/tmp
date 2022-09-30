4217bbfb6f1241cbd0da0a9f04b1d0612d8d2829
Roman Drozd <37835902+roman-drozd-it@users.noreply.github.com>
2022-09-30 17:08:48 +0200

    convert to array-loop

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-console.log("HELLO WORLD");
-console.log("HELLO WORLD");
+// console.log("HELLO WORLD");
+// console.log("HELLO WORLD");
 // ale jaja
 // ok ok
+
+
+[1,2,3,4].forEach(element =>{
+    console.log("HELLOWORLD!")
+});
