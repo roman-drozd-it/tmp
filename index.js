826dde9c60066a4e95766852132f47942803a679
Roman Drozd <37835902+roman-drozd-it@users.noreply.github.com>
2022-09-30 17:40:08 +0200

    import config

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 // ale jaja
 // ok ok
 
+// import config from ('./config/config.json')
+var config = require('./config/config.json')
 
 [1,2,3,4].forEach(element =>{
-    console.log("HELLO WORLD!")
+    console.log(config)
 });
